cfee2d64f80421763da7792afa7a4853d1f5afe1
N1t4z0 <114629649+N1t4z0@users.noreply.github.com>
2023-07-30 23:12:32 -0300

    update

--- laravel/resources/views/chirps/index.blade.php
+++ laravel/resources/views/chirps/index.blade.php
@@ -3,7 +3,7 @@
         <form method="POST" action="{{ route('chirps.store') }}">
             @csrf
             <textarea
-                name="message"
+                name="messages"
                 placeholder="{{ __('What\'s on your mind?') }}"
                 class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
             >{{ old('message') }}</textarea>
